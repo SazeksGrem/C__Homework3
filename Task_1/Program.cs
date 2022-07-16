5b385dc7580646b2813e6e687f1022701efff778
Aleksander Arsentev <sazeks.gremory@gmail.com>
2022-07-16 16:51:34 +0300

    Задания от 1 до 3

---
+++ Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Ведите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+{
+
+    int lastNum = num % 10;
+    int firstNum = num / 10000;
+    if (firstNum == lastNum)
+    {
+        lastNum = (num % 100) / 10;
+        firstNum = (num / 1000) % 10;
+        if (firstNum == lastNum) Console.WriteLine($"{num} -> Да");
+        else Console.WriteLine($"{num} -> Нет");
+    }
+    else Console.WriteLine($"{num} -> Нет");
+
+
+}
+
